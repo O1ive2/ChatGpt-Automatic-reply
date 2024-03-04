4e2bfd770ce17060dfcc6647c3b50253972e9c12
Oliver <2799274183@qq.com>
2024-03-04 18:41:24 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OpenAI } from 'openai';
 
 async function bootstrap() {
+  const openai = new OpenAI({
+    apiKey: 'sk-JGuglLSulwJ0XIZkW7PuY3ruGULyzsdXexHNb8KALxAjkIjC',
+  });
   const app = await NestFactory.create(AppModule);
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+    model: 'gpt-3.5-turbo',
+  });
+  console.log(completion.choices[0]);
   await app.listen(3000);
 }
 bootstrap();
